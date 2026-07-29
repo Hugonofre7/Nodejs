ea4c39db229dd6ad37b692aa6d9590b8a3026c69
Hugonofre <hugoant7@gmail.com>
2026-07-28 23:07:16 -0600

    add log generator and manual backpressure pipeline

---
+++ nodejs-backend/03-streams/backpressure_log_pipeline.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+
+const readline = require('readline');
+
+const levels = ['ERROR', 'INFO', 'DEBUG'];
+
+const services = [
+    'auth-service',
+    'user-service',
+    'payment-service'
+];
+
+const messages = [
+    'Request timeout after 5000ms',
+    'User authenticated successfully',
+    'Processing payment request'
+];
+
+const logs = [];
+    for (let i = 0; i < 200000; i++) {
+
+        const timestamp = new Date().toISOString();
+
+        const level = levels[i % levels.length];
+
+        const service = services[i % services.length];
+
+        const message = messages[i % messages.length];
+
+        const logLine = `${timestamp} ${level} ${service} ${message}`;
+
+        logs.push(logLine);
+}
+
+const content = logs.join('\n') + '\n';
+
+fs.writeFileSync('server_logs.txt',content);
+
+console.log('Archivo de logs generado');
+
+
+function createLogPipeline(inputPath, outputPath) {
+    const startTime = Date.now()
+    const readStream = fs.createReadStream(inputPath, {
+        encoding: 'utf8'
+    });
+
+    const writeStream = fs.createWriteStream(outputPath);
+
+
+    const rl = readline.createInterface({
+        input: readStream,
+        crlfDelay: Infinity
+    });
+
+    rl.on('line', line => {
+
+        if (line.includes('ERROR')) {
+
+            const canContinue = writeStream.write(line + '\n');
+
+            if (!canContinue) {
+                rl.pause();
+            }
+        }
+    });
+
+    writeStream.on('drain', () => {
+
+        rl.resume();
+
+    });
+
+    rl.on('close', () => {
+
+        writeStream.end();
+
+        console.log('Pipeline finalizado');
+
+    });
+
+    rl.on('close', () => {
+        writeStream.end()
+        console.log(`Pipeline finalizado en ${Date.now() - startTime}ms`)
+    })
+
+}
+
+createLogPipeline(
+    'server_logs.txt',
+    'error_logs.txt'
+);
