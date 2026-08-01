be6db8b8fa8f2edff5ef6d3d37c1e726f0f1f55c
Hugonofre <hugoant7@gmail.com>
2026-07-31 23:56:26 -0600

    restart crashed cluster workers automatically

--- nodejs-backend/04-scalability/cluster_load_balancer.js
+++ nodejs-backend/04-scalability/cluster_load_balancer.js
@@ -14,10 +14,39 @@ if (cluster.isPrimary) {
         cluster.fork();
 
 }
+    cluster.on('exit', (worker, code, signal) => {
+
+        console.log(
+            `Worker ${worker.process.pid} murió`
+       );
+
+        console.log(
+            `Código: ${code}, Señal: ${signal}`
+        );
+
+
+        console.log('Creando nuevo worker...');
+
+        cluster.fork();
+
+});
 
 } else {
 
     const server = http.createServer((req, res) => {
+
+    if (req.url === '/crash') {
+
+        console.log(
+            `Worker ${process.pid} simulando crash`
+        );
+
+        process.exit(1);
+
+    }
+
+
+    if (req.url === '/') {
 
         res.writeHead(200, {
             'Content-Type': 'text/plain'
@@ -27,8 +56,9 @@ if (cluster.isPrimary) {
             `Respuesta desde Worker ${process.pid}\n`
         );
 
-    });
+    }
 
+});
 
     server.listen(3000, () => {
 
